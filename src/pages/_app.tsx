621d086211f7b97ab91689b289a251062bff257a
Gustavo Rodrigues <Gustavorr001@gmail.com>
2023-06-30 11:36:25 -0300

    added app logo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import { globalStyles } from '@/styles/global'
 import { Container, Header } from '@/styles/pages/app'
 import type { AppProps } from 'next/app'
+import logoImg from '../assets/logo.png'
+import Image from 'next/image'
 
 globalStyles()
 
@@ -8,7 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <span>LOGO</span>
+        <Image src={logoImg} alt="" />
       </Header>
       <Component {...pageProps} />
     </Container>
